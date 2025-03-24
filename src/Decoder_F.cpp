569cb00eff704b7236ab8b0e181fad1ccbac1ffc
Harshit Kansal <harshitkansal25@gmail.com>
2025-03-24 23:01:05 +0530

    minor bug fix

--- src/Decoder_F.cpp
+++ src/Decoder_F.cpp
@@ -665,8 +665,8 @@ else if (opcode == "1100111" && instr.substr(17, 3) == "000")
         imm_val |= 0xFFFFF000;
     }
     ID.Imm = imm_val;
-
-    IF.branchPC = RegFile[ID.RR1].value + ID.Imm; // Jump target
+    cout << ID.Imm << "gi" << endl;
+    IF.branchPC = RegFile[ID.RR1].value + ID.Imm/4; // Jump target
     IF.branch = 1;
     if (ID.WR != 0)
     RegFile[ID.WR].value = IF.PC; // Save the return address in rd
